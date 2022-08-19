8f65b14258d88567c44075d2c5b3a980da88cb5c
ELIUS <eliuswanyoro@gmail.com>
2022-08-19 10:07:20 +0300

    Completed Tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby' ]
+
+  const findMatching = (s,n) => s.filter(s => s.toLowerCase() === n.toLowerCase());
+
+  const fuzzyMatch = (s,n) => s.filter(s =>s.charAt(0)=== n.charAt(0) );
+
+  const matchName =(s,n) => s.filter(s=> s.name === n);
